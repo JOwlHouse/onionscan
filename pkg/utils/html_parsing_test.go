dc4bfb21baf17bcecf758ab8b32c06136f03578e
JOwlHouse <jgartner@owlhouse.io>
2022-08-06 09:28:28 -0700

    update utils and testing

---
+++ pkg/utils/html_parsing_test.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"strings"
+	"testing"
+
+	"golang.org/x/net/html"
+)
+
+func TestGetAttribute(t *testing.T) {
+	z := html.NewTokenizer(strings.NewReader("<test a=\"b\" c=\"d\">"))
+	z.Next()
+	tok := z.Token()
+	if GetAttribute(tok, "a") != "b" {
+		t.Errorf("Attribute a should have value b")
+	}
+	if GetAttribute(tok, "b") != "" {
+		t.Errorf("Attribute b is missing so should return empty value")
+	}
+}
